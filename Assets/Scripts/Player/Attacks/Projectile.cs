d5ebcc08de976697b8f3b52a57fcf0a4187a0c0d
Jared Osorno <angel.osorno@uniat.edu.mx>
2024-09-25 13:13:30 -0600

    implement

--- Assets/Scripts/Player/Attacks/Projectile.cs
+++ Assets/Scripts/Player/Attacks/Projectile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -5,7 +6,7 @@ public class Projectile : MonoBehaviour
     //Puede que este codigo no haga falta despues
     [SerializeField] private float lifeTime = 3;
     [SerializeField] private int damage ;
-
+    [SerializeField] private List<AudioSource> sounds;
     private void Awake()
     {
         Destroy(gameObject, lifeTime);
@@ -13,11 +14,14 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Map")){
+        if (collision.gameObject.CompareTag("Map"))
+        {
+            sounds[0].Play();
             Destroy(gameObject);
         }
         else if (collision.gameObject.CompareTag("Enemy"))
         {
+            sounds[1].Play();
             collision.gameObject.GetComponent<EnemyStats>().Life = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>().Damage;
             Destroy(gameObject) ;
         }
